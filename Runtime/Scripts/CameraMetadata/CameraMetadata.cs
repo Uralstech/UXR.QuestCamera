c69cf57a0dab7c28c58c7542f92f8c26c088a4ad
Udayshankar R <udayshankar.cto@uralstech.in>
2026-05-18 06:33:39 +0530

    4.1.0-preview.1

---
+++ Runtime/Scripts/CameraMetadata/CameraMetadata.cs
@@ -0,0 +1,178 @@
+﻿// Copyright 2026 URAV ADVANCED LEARNING SYSTEMS PRIVATE LIMITED
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+#nullable enable
+namespace Uralstech.UXR.QuestCamera
+{
+    /// <summary>Represents a map of camera device, capture request or capture result metadata.</summary>
+    public abstract class CameraMetadata : IDisposable
+    {
+        /// <summary>The native object.</summary>
+        public readonly AndroidJavaObject Native;
+
+        /// <summary>The Java/Kotlin class of the native object.</summary>
+        public readonly AndroidJavaClass Class;
+
+        private bool _disposed;
+
+        /// <summary>A range of two integer values.</summary>
+        /// <param name="Lower">The lower endpoint.</param>
+        /// <param name="Upper">The upper endpoint.</param>
+        public sealed record IntRange(int Lower, int Upper);
+
+        /// <summary>A range of two long values.</summary>
+        /// <inheritdoc cref="IntRange"/>
+        public sealed record LongRange(long Lower, long Upper);
+
+        /// <summary>A range of two float values.</summary>
+        /// <inheritdoc cref="IntRange"/>
+        public sealed record FloatRange(float Lower, float Upper);
+
+        /// <summary>
+        /// Light wrapper for <a href="https://developer.android.com/reference/android/hardware/camera2/CaptureRequest.Key">CaptureRequest.Key</a>,
+        /// <a href="https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics.Key">CameraCharacteristics.Key</a>, etc.
+        /// </summary>
+        public sealed class Key : IDisposable, IEquatable<Key>
+        {
+            /// <summary>The native object.</summary>
+            public readonly AndroidJavaObject Native;
+
+            /// <summary>The name of the key.</summary>
+            public readonly string Name;
+
+            private bool _disposed;
+
+            public Key(AndroidJavaObject native)
+            {
+                Native = native;
+                Name = native.Call<string>("getName");
+            }
+
+            /// <inheritdoc/>
+            public override string ToString() => Name;
+
+            /// <inheritdoc/>
+            public override bool Equals(object? obj) => Equals(obj as Key);
+
+            /// <inheritdoc/>
+            public bool Equals(Key? other) => other != null && string.Compare(other.Name, Name, StringComparison.Ordinal) == 0;
+
+            /// <inheritdoc/>
+            public override int GetHashCode() => Name.GetHashCode();
+
+            /// <inheritdoc/>
+            public void Dispose()
+            {
+                if (_disposed)
+                    return;
+
+                _disposed = true;
+                Native.Dispose();
+            }
+        }
+
+        public CameraMetadata(AndroidJavaObject native, string className)
+        {
+            Class = new AndroidJavaClass(className);
+            Native = native;
+        }
+
+        /// <summary>Gets a constant value from <a href="https://developer.android.com/reference/android/hardware/camera2/CameraMetadata">android.hardware.camera2.CameraMetadata</a>.</summary>
+        /// <param name="name">The name of the constant field.</param>
+        public int GetConstant(string name)
+        {
+            ThrowIfDisposed();
+            return Class.GetStatic<int>(name);
+        }
+
+        /// <summary>Returns the keys contained in this map.</summary>
+        public Key[] GetKeys()
+        {
+            ThrowIfDisposed();
+            return GetKeysFromListNative("getKeys");
+        }
+
+        protected Key[] GetKeysFromListNative(string methodName)
+        {
+            using AndroidJavaObject nativeKeysList = Native.Call<AndroidJavaObject>(methodName);
+            AndroidJavaObject[] nativeKeys = nativeKeysList.ConvertList<AndroidJavaObject>();
+            return Array.ConvertAll(nativeKeys, static nativeKey => new Key(nativeKey));
+        }
+
+        /// <summary>Tries to get a value from this map.</summary>
+        /// <remarks>Supports converting to the types supported by <see cref="JNIExtensions.Convert{T}(AndroidJavaObject)"/> + AndroidJavaObject.</remarks>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="keyName">The name of the key.</param>
+        /// <param name="value">The result.</param>
+        /// <returns><see langword="true"/> if the key exists and was converted successfully into a managed type, <see langword="false"/> otherwise.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown if the key is not defined in the native class.</exception>
+        public bool TryGet<T>(string keyName, [MaybeNullWhen(false)] out T value)
+        {
+            ThrowIfDisposed();
+            using AndroidJavaObject key = Class.GetStatic<AndroidJavaObject>(keyName)
+                ?? throw new KeyNotFoundException($"Camera metadata key not found: {keyName}");
+
+            return TryGet(key, out value);
+        }
+
+        /// <summary>Tries to get a value from this map.</summary>
+        /// <remarks>Supports converting to the types supported by <see cref="JNIExtensions.Convert{T}(AndroidJavaObject)"/> + AndroidJavaObject.</remarks>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The result.</param>
+        /// <returns><see langword="true"/> if the key exists and was converted successfully into a managed type, <see langword="false"/> otherwise.</returns>
+        public bool TryGet<T>(Key key, [MaybeNullWhen(false)] out T value)
+        {
+            ThrowIfDisposed();
+            return TryGet(key.Native, out value);
+        }
+
+        private bool TryGet<T>(AndroidJavaObject nativeKey, [MaybeNullWhen(false)] out T value)
+        {
+            if (typeof(T) == typeof(AndroidJavaObject))
+            {
+                AndroidJavaObject? nativeValue = Native.Call<AndroidJavaObject>("get", nativeKey);
+                value = (T?)(object?)nativeValue;
+                return nativeValue != null;
+            }
+
+            value = default;
+
+            using AndroidJavaObject? native = Native.Call<AndroidJavaObject>("get", nativeKey);
+            return native != null && native.TryConvert(out value);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            Class.Dispose();
+            Native.Dispose();
+            _disposed = true;
+        }
+
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(GetType().Name);
+        }
+    }
+}
--- Runtime/Scripts/Extensions/JNIExtensions.cs
+++ Runtime/Scripts/Extensions/JNIExtensions.cs
@@ -13,19 +13,21 @@
 // limitations under the License.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 #nullable enable
 namespace Uralstech.UXR.QuestCamera
 {
-    /// <summary>
-    /// QOL extensions for the JNI.
-    /// </summary>
+    /// <summary>QOL extensions for the JNI.</summary>
+    /// <remarks>
+    /// This class is <see langword="public"/> to allow package users to reuse these extensions if useful.
+    /// However, it should not be considered stable and is not part of the supported public API.
+    /// It exists solely as an internal utility and may change or be removed at any time.
+    /// </remarks>
     public static class JNIExtensions
     {
-        /// <summary>
-        /// Retrieves a ByteBuffer object from a native object array and obtains its direct memory address.
-        /// </summary>
+        /// <summary>Retrieves a ByteBuffer object from a native object array and obtains its direct memory address.</summary>
         /// <param name="args">JNI object array containing the ByteBuffer.</param>
         /// <param name="index">Index of the ByteBuffer within the array.</param>
         /// <returns>A tuple containing the local JNI reference to the ByteBuffer, and the pointer to its direct buffer memory.</returns>
@@ -35,9 +37,7 @@ namespace Uralstech.UXR.QuestCamera
             return (localRef, (IntPtr)AndroidJNI.GetDirectBufferAddress(localRef));
         }
 
-        /// <summary>
-        /// Unboxes an integer from a native Object array.
-        /// </summary>
+        /// <summary>Unboxes an integer from a native Object array.</summary>
         /// <param name="args">The native array to take the integer from.</param>
         /// <param name="index">The index of the integer object in the native array.</param>
         /// <returns>The unboxed integer.</returns>
@@ -50,9 +50,7 @@ namespace Uralstech.UXR.QuestCamera
             return value;
         }
 
-        /// <summary>
-        /// Unboxes a long from a native Object array.
-        /// </summary>
+        /// <summary>Unboxes a long from a native Object array.</summary>
         /// <param name="args">The native array to take the long from.</param>
         /// <param name="index">The index of the long object in the native array.</param>
         /// <returns>The unboxed long.</returns>
@@ -65,24 +63,7 @@ namespace Uralstech.UXR.QuestCamera
             return value;
         }
 
-        /// <summary>
-        /// Unboxes a string from a native Object array.
-        /// </summary>
-        /// <param name="args">The native array to take the string from.</param>
-        /// <param name="index">The index of the string object in the native array.</param>
-        /// <returns>The unboxed string.</returns>
-        public static string? UnboxStringElement(IntPtr args, int index)
-        {
-            IntPtr ptr = AndroidJNI.GetObjectArrayElement(args, index);
-            string? value = AndroidJNI.GetStringUTFChars(ptr);
-
-            AndroidJNI.DeleteLocalRef(ptr);
-            return value;
-        }
-
-        /// <summary>
-        /// Unboxes a boolean from a native Object array.
-        /// </summary>
+        /// <summary>Unboxes a boolean from a native Object array.</summary>
         /// <param name="args">The native array to take the boolean from.</param>
         /// <param name="index">The index of the boolean object in the native array.</param>
         /// <returns>The unboxed boolean.</returns>
@@ -93,6 +74,361 @@ namespace Uralstech.UXR.QuestCamera
 
             AndroidJNI.DeleteLocalRef(ptr);
             return value;
+        }
+
+        /// <summary>Unboxes an AndroidJavaObject from a native Object array.</summary>
+        /// <param name="args">The native array to take the object from.</param>
+        /// <param name="index">The index of the object in the native array.</param>
+        /// <returns>The unboxed object.</returns>
+        public static AndroidJavaObject UnboxObjectElement(IntPtr args, int index)
+        {
+            IntPtr ptr = AndroidJNI.GetObjectArrayElement(args, index);
+
+            try
+            {
+                return new AndroidJavaObject(ptr);
+            }
+            finally
+            {
+                AndroidJNI.DeleteLocalRef(ptr);
+            }
+        }
+
+        /// <summary>Converts a java.util.List to a managed array.</summary>
+        /// <remarks>
+        /// <para>
+        /// WARNING: This method <b>DOES NOT</b> verify if the native Java/Kotlin elements can actually be converted to the target type.
+        /// </para>
+        /// 
+        /// Supports converting elements to the types supported by <see cref="Convert{T}(AndroidJavaObject)"/> + AndroidJavaObject.
+        /// </remarks>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <returns>The converted array.</returns>
+        public static T[] ConvertList<T>(this AndroidJavaObject current)
+        {
+            int length = current.Call<int>("size");
+            T[] result = new T[length];
+
+            bool isAndroidJavaObjectArray = typeof(T) == typeof(AndroidJavaObject);
+            for (int i = 0; i < length; i++)
+            {
+                if (isAndroidJavaObjectArray)
+                {
+                    result[i] = (T)(object)current.Call<AndroidJavaObject>("get", i);
+                    continue;
+                }
+
+                using AndroidJavaObject element = current.Call<AndroidJavaObject>("get", i);
+                result[i] = Convert<T>(element);
+            }
+
+            return result;
+        }
+
+        /// <summary>Safe version of <see cref="Convert{T}(AndroidJavaObject)"/>.</summary>
+        public static bool TryConvert<T>(this AndroidJavaObject current, [MaybeNullWhen(false)] out T? value)
+        {
+            try
+            {
+                value = Convert<T>(current);
+                return true;
+            }
+            catch
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        /// <inheritdoc cref="Convert(AndroidJavaObject, Type)"/>
+        /// <typeparam name="T">The managed target type.</typeparam>
+        public static T Convert<T>(this AndroidJavaObject current) => (T)Convert(current, typeof(T));
+
+        /// <summary>Converts a Java/Kotlin object represented by an <see cref="AndroidJavaObject"/> into a managed .NET/Unity type.</summary>
+        /// <remarks>
+        /// <para>
+        /// WARNING: This method <b>DOES NOT</b> verify if the native Java/Kotlin object can actually be converted to the target type.
+        /// </para>
+        /// 
+        /// Basic supported types:
+        /// <list type="bullet">
+        ///     <item><description><see cref="bool"/></description></item>
+        ///     <item><description><see cref="char"/></description></item>
+        ///     <item><description><see cref="sbyte"/></description></item>
+        ///     <item><description><see cref="short"/></description></item>
+        ///     <item><description><see cref="int"/></description></item>
+        ///     <item><description><see cref="long"/></description></item>
+        ///     <item><description><see cref="float"/></description></item>
+        ///     <item><description><see cref="double"/></description></item>
+        ///     <item><description><see cref="string"/></description></item>
+        /// </list>
+        ///
+        /// Supported Android types:
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Unity Type</term>
+        ///         <description>Android Type</description>
+        ///     </listheader>
+        /// 
+        ///     <item>
+        ///         <term><see cref="Resolution"/></term>
+        ///         <description><a href="https://developer.android.com/reference/android/util/Size">android.util.Size</a></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="Rect"/></term>
+        ///         <description><a href="https://developer.android.com/reference/android/graphics/RectF">android.graphics.RectF</a></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="RectInt"/></term>
+        ///         <description><a href="https://developer.android.com/reference/android/graphics/Rect">android.graphics.Rect</a></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="CameraMetadata.IntRange"/></term>
+        ///         <description><a href="https://developer.android.com/reference/android/util/Range">android.util.Range&lt;Integer&gt;</a></description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="CameraMetadata.LongRange"/></term>
+        ///         <description>android.util.Range&lt;Long&gt;</description>
+        ///     </item>
+        ///     <item>
+        ///         <term><see cref="CameraMetadata.FloatRange"/></term>
+        ///         <description>android.util.Range&lt;Float&gt;</description>
+        ///     </item>
+        /// </list>
+        ///
+        /// Also supports converting arrays of all above types + generic AndroidJavaObject[].
+        /// </remarks>
+        /// <param name="target">The managed target type.</param>
+        /// <returns>The converted managed object.</returns>
+        /// <exception cref="NotSupportedException">Thrown when the requested target type is unsupported.</exception>
+        [SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Code is neater without conditionals.")]
+        public static object Convert(this AndroidJavaObject current, Type target)
+        {
+            if (target.IsPrimitive)
+            {
+                return UnboxPrimitive(current, target);
+            }
+
+            if (target == typeof(string))
+            {
+                return current.Call<string>("toString");
+            }
+
+            if (target.IsArray)
+            {
+                Type elementType = target.GetElementType();
+                if (elementType.IsPrimitive
+                 || elementType == typeof(string)
+                 || elementType == typeof(AndroidJavaObject))
+                {
+                    if (elementType == typeof(byte))
+                        throw new NotSupportedException("Cannot unbox byte[] from Java/Kotlin. Use sbyte[] instead.");
+
+                    return HandleBasicArrays(current, elementType);
+                }
+
+                return HandleCustomArrays(current, elementType);
+            }
+
+            return HandleCustomTypes(current, target);
+        }
+
+        private static Array HandleCustomArrays(AndroidJavaObject current, Type elementType)
+        {
+            IntPtr nativeObj = current.GetRawObject();
+            int length = AndroidJNI.GetArrayLength(nativeObj);
+
+            Array result = Array.CreateInstance(elementType, length);
+            for (int i = 0; i < length; i++)
+            {
+                using AndroidJavaObject element = UnboxObjectElement(nativeObj, i);
+                object converted = Convert(element, elementType);
+                result.SetValue(converted, i);
+            }
+
+            return result;
+        }
+        
+        [SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Code is neater without conditionals.")]
+        private static object HandleCustomTypes(AndroidJavaObject current, Type target)
+        {
+            if (target == typeof(Resolution))
+            {
+                return new Resolution()
+                {
+                    width   = current.Call<int>("getWidth"),
+                    height  = current.Call<int>("getHeight")
+                };
+            }
+
+            if (target == typeof(RectInt))
+            {
+                return new RectInt(
+                    xMin:   current.Get<int>("left"),
+                    yMin:   current.Get<int>("top"),
+                    width:  current.Call<int>("width"),
+                    height: current.Call<int>("height")
+                );
+            }
+
+            if (target == typeof(Rect))
+            {
+                return new Rect(
+                    x:      current.Get<float>("left"),
+                    y:      current.Get<float>("top"),
+                    width:  current.Call<float>("width"),
+                    height: current.Call<float>("height")
+                );
+            }
+
+            if (target == typeof(CameraMetadata.IntRange))
+            {
+                return new CameraMetadata.IntRange(
+                    Lower: current.Call<int>("getLower"),
+                    Upper: current.Call<int>("getUpper")
+                );
+            }
+
+            if (target == typeof(CameraMetadata.LongRange))
+            {
+                return new CameraMetadata.LongRange(
+                    Lower: current.Call<long>("getLower"),
+                    Upper: current.Call<long>("getUpper")
+                );
+            }
+
+            if (target == typeof(CameraMetadata.FloatRange))
+            {
+                return new CameraMetadata.FloatRange(
+                    Lower: current.Call<float>("getLower"),
+                    Upper: current.Call<float>("getUpper")
+                );
+            }
+
+            throw new NotSupportedException($"Type '{target.Name}' is not supported.");
+        }
+
+        [SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Code is neater without conditionals.")]
+        private static object HandleBasicArrays(AndroidJavaObject current, Type elementType)
+        {
+            IntPtr nativeObj = current.GetRawObject();
+
+            if (elementType == typeof(bool))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<bool[]>(nativeObj);
+            }
+
+            if (elementType == typeof(char))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<char[]>(nativeObj);
+            }
+
+            if (elementType == typeof(sbyte))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<sbyte[]>(nativeObj);
+            }
+
+            if (elementType == typeof(byte))
+            {
+                throw new NotSupportedException("Cannot unbox byte[] from Java/Kotlin. Use sbyte[] instead.");
+            }
+
+            if (elementType == typeof(short))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<short[]>(nativeObj);
+            }
+
+            if (elementType == typeof(int))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<int[]>(nativeObj);
+            }
+
+            if (elementType == typeof(long))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<long[]>(nativeObj);
+            }
+
+            if (elementType == typeof(float))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<float[]>(nativeObj);
+            }
+
+            if (elementType == typeof(double))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<double[]>(nativeObj);
+            }
+
+            if (elementType == typeof(string))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<string[]>(nativeObj);
+            }
+
+            if (elementType == typeof(AndroidJavaObject))
+            {
+                return AndroidJNIHelper.ConvertFromJNIArray<AndroidJavaObject[]>(nativeObj);
+            }
+
+            throw new NotSupportedException($"Array with elements of type '{elementType.Name}' is not supported.");
+        }
+
+        private static object UnboxPrimitive(AndroidJavaObject current, Type target)
+        {
+            IntPtr nativeObj = current.GetRawObject();
+
+            if (target == typeof(bool))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out bool value);
+                return value;
+            }
+
+            if (target == typeof(char))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out char value);
+                return value;
+            }
+
+            if (target == typeof(sbyte))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out sbyte value);
+                return value;
+            }
+
+            if (target == typeof(byte))
+            {
+                throw new NotSupportedException("Cannot unbox byte from Java/Kotlin. Use sbyte instead.");
+            }
+
+            if (target == typeof(short))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out short value);
+                return value;
+            }
+
+            if (target == typeof(int))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out int value);
+                return value;
+            }
+
+            if (target == typeof(long))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out long value);
+                return value;
+            }
+
+            if (target == typeof(float))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out float value);
+                return value;
+            }
+
+            if (target == typeof(double))
+            {
+                AndroidJNIHelper.Unbox(nativeObj, out double value);
+                return value;
+            }
+
+            throw new NotSupportedException($"Primitive '{target.Name}' is not supported.");
         }
     }
 }
